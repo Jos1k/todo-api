f7d0dd9a8481cc05535be12e0e254d5e0bc837a7
ihorh <josik4ke@gmail.com>
2016-08-15 23:21:36 +0200

    added repo to package json and added /todos, /todos/:id

--- server.js
+++ server.js
@@ -2,10 +2,44 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.get('/', function(req,res){
+var todos = [{
+    id: 1,
+    description: 'Meet wife in station',
+    completed: false
+},
+    {
+        id: 2,
+        description: 'Call wife shortly',
+        completed: false
+    },
+    {
+        id: 3,
+        description: 'Go to swimming pool',
+        completed: true
+    }];
+
+app.get('/', function (req, res) {
     res.send('TODO API Root');
 });
 
-app.listen(PORT,function(){
+// GET /todos
+app.get('/todos', function (req, res) {
+    res.json(todos);
+});
+
+// GET /todos/:id
+app.get('/todos/:id', function (req, res) {
+    var todo = todos.filter(function (obj) {
+        return obj.id === parseInt(req.params.id);
+    })[0];
+
+    if (!todo) {
+        res.status(404).send('Todo with such id not exist!');
+    }
+
+    res.json(todo);
+});
+
+app.listen(PORT, function () {
     console.log('Express listenting on port ' + PORT + '!');
 })
